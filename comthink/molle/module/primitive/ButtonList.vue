38ab7b76b2243286fbd9e5c85cf7027939d770ea
aokiji516 <h.aoki.web@gmail.com>
2021-07-23 14:47:43 +0900

    #31 Columnのレイアウトのままになっていたので修正（ButtonListとして必要最小限のレイアウトに修正）

--- comthink/molle/module/primitive/ButtonList.vue
+++ comthink/molle/module/primitive/ButtonList.vue
@@ -21,70 +21,11 @@ import {Module} from "~/molle/module/Module";
   components: {},
 })
 export default class ButtonList extends Module {
-  //Unique Methods
-  getClass() {
-    let obj = super.getClass(this.itemData);
-
-    if (this.itemData.option.col) {
-      if (this.itemData.option.col == -1) {
-        obj["column-col"] = true;
-      } else {
-        obj["column-col-" + this.itemData.option.col] = true;
-      }
-    }
-    if (this.itemData.option.colSm) {
-      if (this.itemData.option.colSm == -1) {
-        obj["column-col-sm"] = true;
-      } else {
-        obj["column-col-sm-" + this.itemData.option.colSm] = true;
-      }
-    }
-    return obj;
-  }
-
 }
 </script>
 
 <style lang="scss">
 .button-list {
-  display: flex;
-  flex-wrap: wrap;
-  @include mediaquery-not-sm {
-    margin-top: -2rem;
-    margin-right: -1rem;
-    margin-left: -1rem;
-    margin-bottom: 1rem;
-  }
-  @include mediaquery-sm {
-    margin-top: -1.5rem;
-    margin-right: -0.75rem;
-    margin-left: -0.75rem;
-    margin-bottom: 0.75rem;
-  }
-
-  > * {
-    @include mediaquery-not-sm {
-      padding-top: 2rem;
-      padding-left: 1rem;
-      padding-right: 1rem;
-    }
-    @include mediaquery-sm {
-      padding-top: 1.5rem;
-      padding-left: 0.75rem;
-      padding-right: 0.75rem;
-    }
-  }
-
-  @for $i from 1 through 12 {
-    &-col-#{$i} > * {
-      @include col($i);
-    }
-    @include mediaquery-sm {
-      &-col-sm-#{$i} > * {
-        @include col($i);
-      }
-    }
-  }
 
   &.text-left {
     justify-content: flex-start;
@@ -99,7 +40,7 @@ export default class ButtonList extends Module {
   }
 
   .button {
-    padding: 1rem;
+    margin: 1rem;
   }
 }
 </style>
--- comthink/molle/module/primitive/ButtonListProfile.vue
+++ comthink/molle/module/primitive/ButtonListProfile.vue
@@ -1,30 +1,5 @@
 <template lang="pug">
 div
-  label
-    span.mr-1 基本カラム幅:
-    .form-inline
-      input.form-control.form-control-sm(
-        v-model="itemData.option.col"
-        @change="()=>{$set(itemData.option,'col',Math.max(Math.min(Math.floor(itemData.option.col),12),0));$emit('change')}"
-        type="number"
-        min="0" max="12" step="1",
-        aria-autocomplete="false"
-      )
-      span /12
-  label
-    span.mr-1 基本カラム幅-SP:
-    .form-inline
-      input.form-control.form-control-sm(
-        v-model="itemData.option.colSm"
-        @change="()=>{$set(itemData.option,'colSm',Math.max(Math.min(Math.floor(itemData.option.colSm),12),0));$emit('change')}"
-        type="number"
-        min="0" max="12" step="1"
-        aria-autocomplete="false"
-      )
-      span /12
-  //p.caption *-1=auto
-  p.caption *0=未設定
-
   StyleComp(
     :itemData="itemData"
     @change="()=>$emit('change')"
