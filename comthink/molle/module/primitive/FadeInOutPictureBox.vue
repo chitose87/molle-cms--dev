698183923d947cd55e51f5a680351853cbf8e955
aokiji516 <h.aoki.web@gmail.com>
2021-08-06 16:07:09 +0900

    画面いっぱいに表示されるようにcssを修正

--- comthink/molle/module/primitive/FadeInOutPictureBox.vue
+++ comthink/molle/module/primitive/FadeInOutPictureBox.vue
@@ -31,62 +31,75 @@ export default class FadeInOutPictureBox extends Module {
 </script>
 
 <style lang="scss">
-.fade-in-out-picture-box {
-  position: relative;
-  overflow: hidden;
-  height: 800px;
+.module{
+  margin-top: 0;
 
-  ul {
-    li {
+  .fade-in-out-picture-box {
+    position: relative;
+    overflow: hidden;
+    height: 100vh;
+
+    ul {
+      margin: 0;
       position: absolute;
       top: 0;
       left: 0;
       width: 100%;
-      height: 800px;
-      object-fit: cover;
-      list-style: none;
-      visibility: hidden;
-      animation: anime_slider_fade 12s 0s infinite;
-    }
+      height: 100%;
 
-    li:nth-of-type(2){
-      animation-delay: 4s;
-    }
-
-    li:nth-of-type(3){
-      animation-delay: 8s;
-    }
-
-    @keyframes anime_slider_fade {
-      0%{
-        visibility: visible;
-        opacity: 0;
-      }
-      15%{
-        opacity: 1;
-      }
-      33.3%{
-        opacity: 1;
-        transform: scale(1.05);
-      }
-      48.3%{
-        opacity: 0;
-      }
-      100%{
-        opacity: 0;
-      }
-    }
-
-    .module {
-      margin: 0;
-      picture img{
-        object-fit: cover;
+      li {
+        position: absolute;
+        top: 0;
+        left: 0;
         width: 100%;
-        height: 800px;
+        height: 100%;
+        object-fit: cover;
+        list-style: none;
+        visibility: hidden;
+        animation: anime_slider_fade 12s 0s infinite;
+      }
+
+      li:nth-of-type(2) {
+        animation-delay: 4s;
+      }
+
+      li:nth-of-type(3) {
+        animation-delay: 8s;
+      }
+
+      @keyframes anime_slider_fade {
+        0% {
+          visibility: visible;
+          opacity: 0;
+        }
+        15% {
+          opacity: 1;
+        }
+        33.3% {
+          opacity: 1;
+          transform: scale(1.05);
+        }
+        48.3% {
+          opacity: 0;
+        }
+        100% {
+          opacity: 0;
+        }
+      }
+
+      .module {
+        height: 100%;
+        width: 100%;
+
+        picture img {
+          object-fit: cover;
+          width: 100%;
+          height: 100%;
+        }
+
       }
 
     }
-
   }
 
 }
