50aaad37c65fe71cda3374d079346a8606ee4f73
chitose macoto <chitose87@gmail.com>
2021-08-10 15:19:20 +0900

    update

--- molle-cms/src/ui/PageExport.vue
+++ molle-cms/src/ui/PageExport.vue
@@ -14,7 +14,7 @@ import {
 } from "nuxt-property-decorator";
 import firebase from "firebase";
 import {Singleton} from "molle-cms/src/Singleton";
-import {IPageData} from "~/src/interface";
+import {IPageData} from "molle-cms/src/interface";
 
 @Component({
   components: {},
@@ -22,65 +22,65 @@ import {IPageData} from "~/src/interface";
 export default class PageExport extends Vue {
   @Prop() pageData!: IPageData;
 
-  private totalCount: number = 0;
-  private loopFinishCount: number = 0;
-  private obj: any = {
-    page: {},
-    items: {},
-  };
-
   onExport() {
+    let totalCount = 0;
+    let loopFinishCount = 0;
+    let obj: any = {
+      page: {},
+      items: {},
+    };
+
     // page
-    this.obj.page = this.pageData;
+    obj.page = this.pageData;
     // items
-    this.itemsLoop(this.pageData.itemId, () => {
-      if (this.totalCount === this.loopFinishCount) {
-        this.fileExport();
+    itemsLoop(this.pageData.itemId, () => {
+      if (totalCount === loopFinishCount) {
+
+        //export
+        let a = document.createElement("a");
+        a.href = URL.createObjectURL(
+          new Blob(
+            [JSON.stringify(obj)],
+            {type: "application/json"}),
+        );
+        a.download = `${this.pageData.path}-${this.pageData.title}-${new Date().toUTCString()}.json`;
+        a.click();
       }
     });
-  }
 
-  itemsLoop(_itemId: any, end: any) {
-    this.totalCount++;
-    Singleton.itemsRef.doc(_itemId)
-      .get()
-      .then((snap: firebase.firestore.DocumentSnapshot) => {
-        this.obj.items[_itemId] = snap.data();
-        let itemData: any = snap.data();
+    /**
+     *
+     * @param _itemId
+     * @param end
+     */
+    function itemsLoop(_itemId: any, end: any) {
+      totalCount++;
+      Singleton.itemsRef.doc(_itemId)
+        .get()
+        .then((snap: firebase.firestore.DocumentSnapshot) => {
+          obj.items[_itemId] = snap.data();
+          let itemData: any = snap.data();
 
-        if (itemData.type == "group"
-          ||
-          (itemData.type == "children" && itemData.value.length > 0)) {
-          let count: number = 0;
-          for (let i in itemData.value) {
-            count++;
-            this.itemsLoop(itemData.value[i].id, () => {
-              count--;
-              if (count == 0) {
-                this.loopFinishCount++;
-                end();
-              }
-            });
+          if (itemData.type == "group"
+            || (itemData.type == "children" && itemData.value.length > 0)) {
+            let count: number = 0;
+            for (let i in itemData.value) {
+              count++;
+              itemsLoop(itemData.value[i].id, () => {
+                count--;
+                if (count == 0) {
+                  loopFinishCount++;
+                  end();
+                }
+              });
+            }
+          } else {
+            loopFinishCount++;
+            end();
           }
-        } else {
-          this.loopFinishCount++;
-          end();
-        }
-
-      });
+        });
+    }
   }
-
-  fileExport() {
-    let a = document.createElement("a");
-    a.href = URL.createObjectURL(
-      new Blob(
-        [JSON.stringify(this.obj)],
-        {type: "application/json"}),
-    );
-    a.download = `${this.pageData.path}-${this.pageData.title}-${new Date().toUTCString()}.json`;
-    a.click();
-  }
-
 }
 </script>
 
