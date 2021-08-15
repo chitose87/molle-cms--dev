9f0f0f2d1db68c3b352fb28493dbe4706e84b1a5
aokiji516 <h.aoki.web@gmail.com>
2021-08-15 19:13:41 +0900

    #32 PageExport.vueとPageImport.vueの実装をPagePropertyComp.vueに移動

--- molle-cms/src/ui/PagePropertyComp.vue
+++ molle-cms/src/ui/PagePropertyComp.vue
@@ -76,26 +76,44 @@
 
       hr
 
-      PageExport(:pageData = "pageData")
-      PageImport(:pageData = "pageData" :pageId = "pageId")
+      .mb-2
+        button.btn.btn-info(type="button" @click="onExport")
+          span Export
+      .mb-2
+        button.btn.btn-warning(type="submit" @click="importModal=true")
+          span Import
+        .modal(v-if="importModal" :aria-expanded="importModal")
+          .modal__fiexd
+            .modal__close(@click="importModal=!importModal")
+            .modal__body
+              form(@submit.prevent, @submit="onImport")
+                input(
+                  type="file",
+                  name="files",
+                  accept="application/json",
+                )
+                button.btn.btn-info(type="submit")
+                  span Import
 
 </template>
 
 <script lang="ts">
 import {Component, Vue, Watch, Prop,} from "nuxt-property-decorator";
-import {IPageData} from "molle-cms/src/interface";
+import {IItemData, INodeObject, IPageData} from "molle-cms/src/interface";
 import {Singleton} from "molle-cms/src/Singleton";
 import GoogleStorage from "molle-cms/src/ui/GoogleStorage.vue";
 import InputUrlByGS from "molle-cms/src/ui/property/InputUrlByGS.vue";
-import PageExport from "molle-cms/src/ui/PageExport.vue";
-import PageImport from "molle-cms/src/ui/PageImport.vue";
+import firebase from "firebase";
+import {Utils} from "molle-cms/src/Utils";
 
 @Component({
-  components: {InputUrlByGS, GoogleStorage, PageExport, PageImport},
+  components: {InputUrlByGS, GoogleStorage},
 })
 export default class PagePropertyComp extends Vue {
   @Prop() pageData!: IPageData;
   @Prop() pageId!: string;
+
+  importModal: boolean = false;
 
   @Watch("pageData", {immediate: true})
   onChangePageData(newer: string, older?: string) {
@@ -104,6 +122,147 @@ export default class PagePropertyComp extends Vue {
   update() {
     Singleton.pagesRef.doc(this.pageId).update(this.pageData);
   }
+
+  onExport() {
+    let totalCount = 0;
+    let loopFinishCount = 0;
+    let obj: any = {
+      page: {},
+      items: {},
+    };
+
+    // page
+    obj.page = this.pageData;
+    // items
+    itemsLoop(this.pageData.itemId, () => {
+      if (totalCount === loopFinishCount) {
+
+        //export
+        let a = document.createElement("a");
+        a.href = URL.createObjectURL(
+          new Blob(
+            [JSON.stringify(obj)],
+            {type: "application/json"}),
+        );
+        a.download = `${this.pageData.path}-${this.pageData.title}-${new Date().toUTCString()}.json`;
+        a.click();
+      }
+    });
+
+    /**
+     *
+     * @param _itemId
+     * @param end
+     */
+    function itemsLoop(_itemId: any, end: any) {
+      totalCount++;
+      Singleton.itemsRef.doc(_itemId)
+        .get()
+        .then((snap: firebase.firestore.DocumentSnapshot) => {
+          obj.items[_itemId] = snap.data();
+          let itemData: any = snap.data();
+
+          if (itemData.type == "group"
+            || (itemData.type == "children" && itemData.value.length > 0)) {
+            let count: number = 0;
+            for (let i in itemData.value) {
+              count++;
+              itemsLoop(itemData.value[i].id, () => {
+                count--;
+                if (count == 0) {
+                  loopFinishCount++;
+                  end();
+                }
+              });
+            }
+          } else {
+            loopFinishCount++;
+            end();
+          }
+        });
+    }
+  }
+
+  onImport(e: Event) {
+    let target = <HTMLFormElement>e.target;
+    let files: FileList = target.files.files;
+
+    if (files.length == 0) {
+      alert("ファイルが選択されていません。");
+      return;
+    }
+
+    let reader = new FileReader();
+    reader.onload = (e: any) => {
+      try {
+        let data = JSON.parse(e.target.result);
+        if (!data.items) {
+          alert("ファイル内にitemsDataが存在しません。");
+          return;
+        }
+        if (!data.page) {
+          alert("ファイル内にpageDataが存在しません。");
+          return;
+        }
+
+        let createTime = firebase.firestore.Timestamp.now();
+        let batchQue: any = [];
+
+        //items import
+        let loopReplaceUpload = (node: INodeObject) => {
+          let _newId = Singleton.itemsRef.doc().id;
+          let item = <IItemData>data.items[node.id];
+          if (item.type == "group" || item.type == "children") {
+            for (let i in item.value) {
+              // console.log(item.value[i])
+              item.value[i].id = loopReplaceUpload(item.value[i]);
+            }
+          }
+
+          // itemSet
+          item.createTime = createTime;
+          batchQue.push({
+            cmd: "set",
+            ref: Singleton.itemsRef.doc(_newId),
+            data: item,
+          });
+
+          // log
+          batchQue.push({
+            cmd: "set",
+            ref: Singleton.logsRef.doc(_newId),
+            data: {
+              history: [{
+                timestamp: createTime,
+                uid: firebase.auth().currentUser!.uid,
+              }],
+            },
+          });
+          return _newId;
+        };
+
+        //page import
+        data.page.itemId = loopReplaceUpload({id: data.page.itemId});
+        data.page.path = this.pageData.path;
+
+        batchQue.push({
+          cmd: "set",
+          ref: Singleton.pagesRef.doc(this.pageId),
+          data: data.page,
+        });
+
+        // commit
+        Utils.updateBatch(batchQue).then(() => {
+          alert("complete");
+        });
+
+      } catch (e) {
+        alert(e);
+      }
+    };
+    reader.readAsText(files[0]);
+  }
+
 }
 </script>
 
