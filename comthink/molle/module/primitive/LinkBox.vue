417016202f97747b73d23ea0df130a83af7a9309
chitose macoto <chitose87@gmail.com>
2021-08-19 15:02:46 +0900

    LinkBox

--- comthink/molle/module/primitive/LinkBox.vue
+++ comthink/molle/module/primitive/LinkBox.vue
@@ -1,23 +1,21 @@
 <template lang="pug">
-component.module.link-box(
-  :is="itemData.option.tag||'div'",
+a.module.link-box(
   :id="itemData.tagId",
   :class="getClass(itemData)",
   :style="getStyle(itemData)",
+  :href="$route.query.edit ? 'javascript:void(0)' : itemData.option.href"
+  :target="itemData.option.target"
 )
+  ModuleLoader(
+    v-for="node in itemData.value",
+    :key="node.id",
+    :node="node"
+  )
 
-  a.link(:href="$route.query.edit ? 'javascript:void(0)' : itemData.option.href")
-
-    ModuleLoader(
-      v-for="node in itemData.value",
-      :key="node.id",
-      :node="node"
-    )
-
-    AddModuleComp(
-      :label="`Link-Box`"
-      :parentNode="loader.node"
-    )
+  AddModuleComp(
+    :label="`Link-Box`"
+    :parentNode="loader.node"
+  )
 </template>
 
 <script lang="ts">
@@ -33,115 +31,113 @@ export default class LinkBox extends Module {
 
 <style lang="scss">
 .link-box {
-  .link {
-    display: block;
-  }
+  display: block;
 
-  > *:first-child {
-    margin-top: 0;
-  }
-
-  > *:last-child {
-    margin-bottom: 0;
-  }
-
-  //thema
-  &.-quote {
-    background-color: $color-gray-100;
-    padding: 2rem 1rem;
-    position: relative;
-
-    &:before,
-    &::after {
-      position: absolute;
-      font-size: 200%;
-      line-height: 1;
-    }
-
-    &::before {
-      content: "“";
-      left: 0.5rem;
-      top: 1rem;
-    }
-
-    &::after {
-      content: "”";
-      right: 0.5rem;
-      bottom: 0rem;
-    }
-  }
-
-  &.-overlay {
-    background-color: rgba(164, 164, 164, 0.3);
-    position: relative;
-    z-index: 10;
-    @include mediaquery-not-sm {
-      margin: -$gutter*2.5 7% $gutter*2;
-      padding-top: $gutter*2;
-      padding-bottom: $gutter*2;
-
-    }
-    @include mediaquery-sm {
-      padding-top: 3rem;
-      padding-bottom: 3rem;
-    }
-  }
-
-  &.-right {
-    margin-left: 7%;
-    margin-bottom: 8rem;
-    padding: 3.9%;
-    background-color: rgba(164, 164, 164, 0.3);
-  }
-
-  &.-left {
-    margin-bottom: 8rem;
-    padding: 3.9% 3.9% 3.9% 10.9%;
-    background: linear-gradient(to right, rgba(164, 164, 164, 0.3) 0%, rgba(164, 164, 164, 0.3) 93%, #ffffff 93%, #ffffff 100%);
-  }
-
-  &.-sm-right {
-    //margin-left: 7%;
-    //padding: 3.9%;
-    padding: 3.9% 3.9% 3.9% 10.9%;
-    margin-bottom: 8rem;
-    background: linear-gradient(to right, #ffffff 0%, #ffffff 65%, rgba(164, 164, 164, 0.3) 65%, rgba(164, 164, 164, 0.3) 93%, #ffffff 93%, #ffffff 100%,);
-  }
-
-  &.-sm-left {
-    margin-left: 7%;
-    padding: 3.9%;
-    margin-bottom: 8rem;
-    background: linear-gradient(to right, rgba(164, 164, 164, 0.3) 0%, rgba(164, 164, 164, 0.3) 35%, #ffffff 35%, #ffffff 100%);
-  }
-
-  // color
-  &.-gray {
-    background-color: $color-gray-600;
-    @include mediaquery-not-sm {
-      padding-top: $gutter*2;
-      padding-bottom: $gutter*2;
-    }
-    @include mediaquery-sm {
-      padding-top: 3rem;
-      padding-bottom: 3rem;
-    }
-  }
-
-  &.-gray-box {
-    border: 6px solid $color-border-light;
-    padding: 1.5rem;
-    background-color: $color-white;
-    @include mediaquery-sm {
-      padding: 1rem;
-    }
-  }
-
-  &.-dark {
-    background-color: $color-black;
-    color: $color-text-white;
-    padding-top: $gutter*2;
-    padding-bottom: $gutter*2;
-  }
+  //> *:first-child {
+  //  margin-top: 0;
+  //}
+  //
+  //> *:last-child {
+  //  margin-bottom: 0;
+  //}
+  //
+  ////thema
+  //&.-quote {
+  //  background-color: $color-gray-100;
+  //  padding: 2rem 1rem;
+  //  position: relative;
+  //
+  //  &:before,
+  //  &::after {
+  //    position: absolute;
+  //    font-size: 200%;
+  //    line-height: 1;
+  //  }
+  //
+  //  &::before {
+  //    content: "“";
+  //    left: 0.5rem;
+  //    top: 1rem;
+  //  }
+  //
+  //  &::after {
+  //    content: "”";
+  //    right: 0.5rem;
+  //    bottom: 0rem;
+  //  }
+  //}
+  //
+  //&.-overlay {
+  //  background-color: rgba(164, 164, 164, 0.3);
+  //  position: relative;
+  //  z-index: 10;
+  //  @include mediaquery-not-sm {
+  //    margin: -$gutter*2.5 7% $gutter*2;
+  //    padding-top: $gutter*2;
+  //    padding-bottom: $gutter*2;
+  //
+  //  }
+  //  @include mediaquery-sm {
+  //    padding-top: 3rem;
+  //    padding-bottom: 3rem;
+  //  }
+  //}
+  //
+  //&.-right {
+  //  margin-left: 7%;
+  //  margin-bottom: 8rem;
+  //  padding: 3.9%;
+  //  background-color: rgba(164, 164, 164, 0.3);
+  //}
+  //
+  //&.-left {
+  //  margin-bottom: 8rem;
+  //  padding: 3.9% 3.9% 3.9% 10.9%;
+  //  background: linear-gradient(to right, rgba(164, 164, 164, 0.3) 0%, rgba(164, 164, 164, 0.3) 93%, #ffffff 93%, #ffffff 100%);
+  //}
+  //
+  //&.-sm-right {
+  //  //margin-left: 7%;
+  //  //padding: 3.9%;
+  //  padding: 3.9% 3.9% 3.9% 10.9%;
+  //  margin-bottom: 8rem;
+  //  background: linear-gradient(to right, #ffffff 0%, #ffffff 65%, rgba(164, 164, 164, 0.3) 65%, rgba(164, 164, 164, 0.3) 93%, #ffffff 93%, #ffffff 100%,);
+  //}
+  //
+  //&.-sm-left {
+  //  margin-left: 7%;
+  //  padding: 3.9%;
+  //  margin-bottom: 8rem;
+  //  background: linear-gradient(to right, rgba(164, 164, 164, 0.3) 0%, rgba(164, 164, 164, 0.3) 35%, #ffffff 35%, #ffffff 100%);
+  //}
+  //
+  //// color
+  //&.-gray {
+  //  background-color: $color-gray-600;
+  //  @include mediaquery-not-sm {
+  //    padding-top: $gutter*2;
+  //    padding-bottom: $gutter*2;
+  //  }
+  //  @include mediaquery-sm {
+  //    padding-top: 3rem;
+  //    padding-bottom: 3rem;
+  //  }
+  //}
+  //
+  //&.-gray-box {
+  //  border: 6px solid $color-border-light;
+  //  padding: 1.5rem;
+  //  background-color: $color-white;
+  //  @include mediaquery-sm {
+  //    padding: 1rem;
+  //  }
+  //}
+  //
+  //&.-dark {
+  //  background-color: $color-black;
+  //  color: $color-text-white;
+  //  padding-top: $gutter*2;
+  //  padding-bottom: $gutter*2;
+  //}
 }
 </style>
--- comthink/molle/module/primitive/LinkBoxProfile.vue
+++ comthink/molle/module/primitive/LinkBoxProfile.vue
@@ -1,13 +1,10 @@
 <template lang="pug">
 div
-  label.form-inline
-    span.mr-1 タグ:
-    select.form-control.form-control-sm(
-      v-model="itemData.option.tag"
-      @change="()=>$emit('change')"
-    )
-      option(v-for="item in ['', 'section']" :value="item" v-html="item")
-
+  StyleComp(
+    :itemData="itemData"
+    @change="()=>$emit('change')"
+    :permission="stylePermission"
+  )
   label.form-inline
     span.mr-1 URL:
     input.form-control.form-control-sm(
@@ -15,11 +12,13 @@ div
       @change="()=>$emit('change')"
     )
 
-  StyleComp(
-    :itemData="itemData"
-    @change="()=>$emit('change')"
-    :permission="stylePermission"
-  )
+  label.form-inline
+    span.mr-1 ターゲット:
+    select.form-control.form-control-sm(
+      v-model="itemData.option.target"
+      @change="()=>$emit('change')"
+    )
+      option(v-for='item in ["", "_self", "_blank"]' :value="item" v-html="item")
 
 </template>
 
@@ -29,19 +28,16 @@ import StyleComp from "molle-cms/src/ui/property/StyleComp.vue";
 import {Profile} from "molle-cms/src/module/Profile";
 
 @Component({
-  components: {StyleComp}
+  components: {StyleComp},
 })
 export default class LinkBoxProfile extends Profile {
   //style setting
   stylePermission = {
-    container: false,
-    "container-fluid": false,
-    section: false,
-    border: false,
-    margin: "",
-    padding: "",
-    theme: {default: "", select: ["", "-overlay", "-right", "-left", "-sm-right", "-sm-left", "-quote"]},
-    color: {default: "", select: ["", "-dark", "-gray", "-gray-box"]},
+    // border: false,
+    // margin: "",
+    // padding: "",
+    // theme: {default: "", select: ["", "-overlay", "-right", "-left", "-sm-right", "-sm-left", "-quote"]},
+    // color: {default: "", select: ["", "-dark", "-gray", "-gray-box"]},
   };
 }
 </script>
