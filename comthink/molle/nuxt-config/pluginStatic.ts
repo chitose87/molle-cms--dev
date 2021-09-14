61d783564035b4d6913a65d659f8f208006e952e
chitose macoto <chitose87@gmail.com>
2021-09-14 14:01:41 +0900

    moduleのDefault化

--- comthink/molle/nuxt-config/pluginStatic.ts
+++ comthink/molle/nuxt-config/pluginStatic.ts
@@ -5,28 +5,28 @@
 
 import {Vue} from "nuxt-property-decorator";
 /* <imports */
-import Box from "~/molle/module/primitive/Box.vue"
-import BackgroundBox from "~/molle/module/primitive/BackgroundBox.vue"
-import Headline from "~/molle/module/primitive/Headline.vue"
-import Paragraph from "~/molle/module/primitive/Paragraph.vue"
-import Picture from "~/molle/module/primitive/Picture.vue"
-import Button from "~/molle/module/primitive/Button.vue"
-import Column from "~/molle/module/primitive/Column.vue"
-import ColumnBox from "~/molle/module/primitive/ColumnBox.vue"
-import Gallery from "~/molle/module/custom/Gallery.vue"
-import GoogleForm from "~/molle/module/custom/GoogleForm.vue"
-import SocialShare from "~/molle/module/custom/SocialShare.vue"
-import Modal from "~/molle/module/primitive/Modal.vue"
-import Card from "~/molle/module/custom/Card.vue"
-import GoogleMap from "~/molle/module/custom/GoogleMap.vue"
-import Embed from "~/molle/module/primitive/Embed.vue"
-import LinkBox from "~/molle/module/primitive/LinkBox.vue"
-import IsviewBox from "~/molle/module/primitive/IsviewBox.vue"
-import ButtonList from "~/molle/module/primitive/ButtonList.vue"
 import SlideShow from "~/molle/module/custom/SlideShow.vue"
 import Feature from "~/molle/module/custom/Feature.vue"
 import FeatureBox from "~/molle/module/custom/FeatureBox.vue"
-import Table from "~/molle/module/primitive/Table.vue"
+import Box from "~/molle-cms/src/module/primitive/Box.vue"
+import BackgroundBox from "~/molle-cms/src/module/primitive/BackgroundBox.vue"
+import Headline from "~/molle-cms/src/module/primitive/Headline.vue"
+import Paragraph from "~/molle-cms/src/module/primitive/Paragraph.vue"
+import Picture from "~/molle-cms/src/module/primitive/Picture.vue"
+import Button from "~/molle-cms/src/module/primitive/Button.vue"
+import Column from "~/molle-cms/src/module/primitive/Column.vue"
+import ColumnBox from "~/molle-cms/src/module/primitive/ColumnBox.vue"
+import Embed from "~/molle-cms/src/module/primitive/Embed.vue"
+import LinkBox from "~/molle-cms/src/module/primitive/LinkBox.vue"
+import IsviewBox from "~/molle-cms/src/module/primitive/IsviewBox.vue"
+import ButtonList from "~/molle-cms/src/module/primitive/ButtonList.vue"
+import Modal from "~/molle-cms/src/module/primitive/Modal.vue"
+import Gallery from "~/molle-cms/src/module/custom/Gallery.vue"
+import GoogleForm from "~/molle-cms/src/module/custom/GoogleForm.vue"
+import SocialShare from "~/molle-cms/src/module/custom/SocialShare.vue"
+import Card from "~/molle-cms/src/module/custom/Card.vue"
+import GoogleMap from "~/molle-cms/src/module/custom/GoogleMap.vue"
+import Table from "~/molle-cms/src/module/primitive/Table.vue"
 /* > */
 import ModuleLoader from "~/molle/module/ModuleLoader.vue";
 import Blank from "~/molle-cms/src/Blank.vue";
@@ -40,16 +40,16 @@ Vue.component("Picture", Picture);
 Vue.component("Button", Button);
 Vue.component("Column", Column);
 Vue.component("ColumnBox", ColumnBox);
-Vue.component("Gallery", Gallery);
-Vue.component("GoogleForm", GoogleForm);
-Vue.component("SocialShare", SocialShare);
-Vue.component("Modal", Modal);
-Vue.component("Card", Card);
-Vue.component("GoogleMap", GoogleMap);
 Vue.component("Embed", Embed);
 Vue.component("LinkBox", LinkBox);
 Vue.component("IsviewBox", IsviewBox);
 Vue.component("ButtonList", ButtonList);
+Vue.component("Modal", Modal);
+Vue.component("Gallery", Gallery);
+Vue.component("GoogleForm", GoogleForm);
+Vue.component("SocialShare", SocialShare);
+Vue.component("Card", Card);
+Vue.component("GoogleMap", GoogleMap);
 Vue.component("SlideShow", SlideShow);
 Vue.component("Feature", Feature);
 Vue.component("FeatureBox", FeatureBox);
@@ -58,4 +58,5 @@ Vue.component("Table", Table);
 
 Vue.component("MolleEditerComp", Blank);
 Vue.component("AddModuleComp", Blank);
+Vue.component("DevModuleGuide", Blank);
 Vue.component("ModuleLoader", ModuleLoader);
