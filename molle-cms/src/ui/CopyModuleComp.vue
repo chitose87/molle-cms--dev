92bae3f9dbd06c2d8033fc6cf09a09221cc2acce
aokiji516 <h.aoki.web@gmail.com>
2021-12-20 13:01:00 +0900

    修正

--- molle-cms/src/ui/CopyModuleComp.vue
+++ molle-cms/src/ui/CopyModuleComp.vue
@@ -3,7 +3,7 @@
   .text-right
     span.small.mr-2 選択中のモジュールを
     button.btn.btn-sm.btn-outline-info(type="button", @click="onClone") {{$words.copy}}
-    div(v-if="copyItem.key === 'KeyC' || (copyItem.key === 'KeyX' && !isEnabled)")
+    div(v-if="copyItem.key === 'KeyC' || (copyItem.key === 'KeyX' && !enabled)")
       hr
       span.small.mr-2 {{copyItem.id}}を
       button.btn.btn-sm.btn-outline-info(
@@ -32,18 +32,18 @@ export default class CopyModuleComp extends Vue {
   @Prop() afterNode?: INodeObject;
 
   private copyItem = MoUtils.ls.copyItem;
-  private isEnabled: boolean = false;
+  private enabled: boolean = false;
 
   mounted() {
     // 切り取り元モジュールのbefore/afterが選択されている場合、true
-    this.isEnabled = this.copyItem.id == (this.beforeNode! || this.afterNode!).id;
-    if (this.isEnabled) return;
+    this.enabled = this.copyItem.id == (this.beforeNode! || this.afterNode!).id;
+    if (this.enabled) return;
 
     // 切り取り元モジュール配下のbefore/afterが選択されている場合、true
     let loop = (id: string) => {
       let parent: any = ModuleLoaderCms.modules[id].$parent;
-      this.isEnabled = this.copyItem.id == parent.$parent.node.id;
-      if(this.isEnabled || parent.$parent.$parent.pageData) return;  // trueまたは次の親がなくなったらloop終了
+      this.enabled = this.copyItem.id == parent.$parent.node.id;
+      if(this.enabled || parent.$parent.$parent.pageData) return;  // trueまたは次の親がなくなったらloop終了
       // 次の親をチェック
       loop(parent.$parent.node.id)
     };
